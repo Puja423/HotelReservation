b73f946ae5e5f74c4f01545e500c607093e3d0e9
Puja <pujayadav@gmail.com>
2020-11-07 09:46:33 +0530

    [puja] add . cheapest Best Radted Hotel

--- HotelReservation/CheapestHotel.cs
+++ HotelReservation/CheapestHotel.cs
@@ -4,34 +4,59 @@ using System.Text;
 
 namespace HotelReservation
 {
-   public class CheapestHotel
+    class CustomerServices
     {
-        public void FindCheapHotel(string startDate, string endDate)
+        /// <summary>
+        /// Finds Cheapest Hotel with best rating
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        public void FindCheapHotel(string startDate, string endDate, CustomerType customerType)
         {
-            HotelName hotelName = HotelName.Lakewood;
-            Hotel lakewood = new Hotel(hotelName);
+            HotelType hotelType = HotelType.LAKEWOOD;
+            Hotel lakewood = new Hotel(hotelType, customerType);
             double rateLakewood = lakewood.FindRate(startDate, endDate);
-            hotelName = HotelName.Bridgewood;
-            Hotel bridgewood = new Hotel(hotelName);
+            hotelType = HotelType.BRIDGEWOOD;
+            Hotel bridgewood = new Hotel(hotelType, customerType);
             double rateBridgewood = bridgewood.FindRate(startDate, endDate);
-            hotelName = HotelName.Ridgewood;
-            Hotel ridgewood = new Hotel(hotelName);
+            hotelType = HotelType.RIDGEWOOD;
+            Hotel ridgewood = new Hotel(hotelType, customerType);
             double rateRidgewood = ridgewood.FindRate(startDate, endDate);
             if (rateLakewood < rateBridgewood && rateLakewood < rateRidgewood)
             {
-                hotelName = HotelName.Lakewood;
-                Console.WriteLine("hotel" + hotelName + " Cost: " + rateLakewood);
+                Console.WriteLine("Best hotel for your stay is " + HotelType.LAKEWOOD + ", Rating: " + lakewood.RATING + ", Cost of stay: " + rateLakewood);
             }
-            if (rateBridgewood < rateLakewood && rateBridgewood < rateRidgewood)
+            if ((rateBridgewood < rateLakewood && rateBridgewood < rateRidgewood) || (rateLakewood == rateBridgewood && rateBridgewood < rateRidgewood))
             {
-                hotelName = HotelName.Bridgewood;
-                Console.WriteLine("hotel " + hotelName + " Cost: " + rateBridgewood);
+                Console.WriteLine("Best hotel for your stay is " + HotelType.BRIDGEWOOD + ", Rating: " + bridgewood.RATING + ", Cost of stay: " + rateBridgewood);
             }
-            if (rateRidgewood < rateLakewood && rateRidgewood < rateBridgewood)
+            if ((rateRidgewood < rateLakewood && rateRidgewood < rateBridgewood) || (rateLakewood == rateRidgewood && rateRidgewood < rateBridgewood) || (rateBridgewood == rateRidgewood && rateRidgewood < rateLakewood))
             {
-                hotelName = HotelName.Ridgewood;
-                Console.WriteLine("Hote" + hotelName + "Cost:" + rateRidgewood);
+                Console.WriteLine("Best hotel for your stay is " + HotelType.RIDGEWOOD + ", Rating: " + ridgewood.RATING + ", Cost of stay: " + rateRidgewood);
             }
+        }
+        /// <summary>
+        /// Finds best hotel according to rating
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        public void FindBestRatedHotel(string startDate, string endDate, CustomerType customerType)
+        {
+            HotelType hotelType = HotelType.LAKEWOOD;
+            Hotel lakewood = new Hotel(hotelType, customerType);
+            double rateLakewood = lakewood.FindRate(startDate, endDate);
+            hotelType = HotelType.BRIDGEWOOD;
+            Hotel bridgewood = new Hotel(hotelType, customerType);
+            double rateBridgewood = bridgewood.FindRate(startDate, endDate);
+            hotelType = HotelType.RIDGEWOOD;
+            Hotel ridgewood = new Hotel(hotelType, customerType);
+            double rateRidgewood = ridgewood.FindRate(startDate, endDate);
+            if (lakewood.RATING > bridgewood.RATING && lakewood.RATING > ridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.LAKEWOOD + ", Rating: " + lakewood.RATING + ", Cost of stay: " + rateLakewood);
+            if (bridgewood.RATING > lakewood.RATING && bridgewood.RATING > ridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.BRIDGEWOOD + ", Rating: " + bridgewood.RATING + ", Cost of stay: " + rateBridgewood);
+            if (ridgewood.RATING > lakewood.RATING && ridgewood.RATING > bridgewood.RATING)
+                Console.WriteLine("Best hotel for your stay is " + HotelType.RIDGEWOOD + ", Rating: " + ridgewood.RATING + ", Cost of stay: " + rateRidgewood);
         }
     }
 }
